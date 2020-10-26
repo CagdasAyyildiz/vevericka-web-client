aec265bcec8a83d7e8e7ca7de90d25cada855f84
CagdasAyyildiz <cagdasayyildiz.ayyildiz@gmail.com>
2020-10-26 18:47:02 +0300

    "test"

--- src/views/MessagesPage.vue
+++ src/views/MessagesPage.vue
@@ -1,27 +1,60 @@
 <template>
   <v-container class="mx-auto mt-5">
-    <h1>Messages Feature</h1>
-    <h1>Our team works hard to develop this feature.</h1>
-    <h1>
-      See you soon
-      <span class="red--text"> {{ name }} </span>.
-    </h1>
+    <h1>Mert</h1>
+    <input type="submit" value="Submit" @click="selectUser('adminmert')">
+    <h1>Eren</h1><input type="button" value="Submit" @click="newMessage({message: 'Dune', sentBy: 'cagdasay'}) ">
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "MessagesPage",
-  data: () => ({
-    user: {},
-  }),
-  computed: {
-    name() {
-      return this.$store.state.user.name;
-    },
+  data: () => {
+    return {
+      username: 'cagdasay',
+      users: ['cagdasay'],
+      message: "",
+      messages: []
+    }
   },
-};
+  sockets: {
+    oldMessages(data) {
+      this.messages = data;
+      console.log(this.messages);
+    }
+  },
+  methods: {
+    selectUser (name) {
+      this.username = name;
+      console.log(this.username);
+      this.users.push(this.username);
+      console.log(this.users);
+
+      this.$socket.emit('initialize', this.users);
+    },
+    newMessage (data) {
+      this.message = data.message;
+      this.sentBy = data.sentBy;
+      this.messages.push(this.messages);
+      this.$socket.emit('new message', data);
+    },
+  }
+}
 </script>
 
-<style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
